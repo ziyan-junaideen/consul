af26b182ec8ba26c7700bd8077c8d7b8c1c494c6
Ziyan Junaideen <ziyan@jdeen.com>
2018-08-17 09:56:03 +0530

    Related content help text

--- app/helpers/related_contents_helper.rb
+++ app/helpers/related_contents_helper.rb
@@ -1,9 +1,23 @@
 module RelatedContentsHelper
-  def related_content_title_i18n_key(related)
-    if related.is_a?(Budget::Investment) && related.idea?
+  def related_content_title_i18n_key(relationable)
+    if budget_idea?(relationable)
       'budget_idea'
     else
-      related.model_name.singular
+      relationable.model_name.singular
     end
+  end
+
+  def related_content_types(relationable)
+    if budget_idea?(relationable)
+      t('related_content.content_title').select { |k, _| %i[proposal budget_idea].include?(k) }.values.to_sentence
+    else
+      t('related_content.content_title').values.to_sentence
+    end
+  end
+
+  private
+
+  def budget_idea?(relationable)
+    relationable.is_a?(Budget::Investment) && relationable.idea?
   end
 end
