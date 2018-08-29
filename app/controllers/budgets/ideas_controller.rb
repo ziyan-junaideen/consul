4dd2796842a066ed78d84126c9366158edd6ac18
Ziyan Junaideen <ziyan@jdeen.com>
2018-08-29 08:23:14 +0530

    Showing only published

--- app/controllers/budgets/ideas_controller.rb
+++ app/controllers/budgets/ideas_controller.rb
@@ -34,9 +34,9 @@ module Budgets
 
     def index
       if @budget.finished?
-        @investments = investments.idea.winners.page(params[:page]).per(10).for_render
+        @investments = investments.idea.published.winners.page(params[:page]).per(10).for_render
       else
-        @investments = investments.idea.page(params[:page]).per(10).for_render
+        @investments = investments.idea.published.page(params[:page]).per(10).for_render
       end
 
       @investment_ids = @investments.pluck(:id)
