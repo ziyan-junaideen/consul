8805e613aa9692393d2d1b2b0a470bc4520d0e3a
allthesignals <wilburnforce@gmail.com>
2018-09-23 21:20:41 -0400

    Revert "Add secret"
    This reverts commit eef92b797df0d7b29b474a8d6f78241b22af1134.

--- config/initializers/devise_security_extension.rb
+++ config/initializers/devise_security_extension.rb
@@ -1,7 +1,7 @@
 Devise.setup do |config|
   # ==> Security Extension
   # Configure security extension for devise
-  config.secret_key = '4dfcacf9d92d1058cf05bccdcab87f88cfe31e58cd7b75c681a6133af48e4235b4cd48c1a866cd040347688b0e33be6e9e82ff5735fc6acd88d62c5c9fc8dc18'
+
   # Should the password expire (e.g 3.months)
   # config.expire_password_after = false
   config.expire_password_after = 1.year
