48f9e6166459c9ade0fc9155ad10c6cf96322728
Ziyan Junaideen <ziyan@jdeen.com>
2018-10-28 17:41:25 +0530

    Specs + class to handle import

--- lib/budget_idea_importer.rb
+++ lib/budget_idea_importer.rb
@@ -1,63 +1,74 @@
 require 'csv'
 
 class BudgetIdeaImporter
-  def initialize
-    path = Rails.root.join('archives', 'budget_idea_import.csv')
-    @csv = CSV.parse(File.read(path))
+  attr_accessor :heading, :default_author
+
+  delegate :budget, :group, to: :heading
+
+  def initialize(heading, default_author)
+    @heading = heading
+    @default_author = default_author
   end
 
-  def perform
+  def import(file_path)
+    load_data(file_path)
+
     @csv.each_with_index do |row, index|
-      begin
-        budget = budget_from_row(row)
-        heading = heading_from_row(budget, row)
-        author = author_from_row(row)
+      author = author_from_row(row) || default_author
 
-        options = {
-          kind: 'idea',
-          heading: heading,
-          group: heading.group,
-          title: row['Title'],
-          description: row['Description'],
-          author: author,
-          terms_of_service: '1'
-        }
+      options = {
+        kind: 'idea',
+        heading: heading,
+        group: group,
+        title: row['Title'],
+        description: row['Description'],
+        author: author,
+        terms_of_service: '1',
+        location: row['Location']
+      }
 
-        add_map(options, row)
+      add_map(options, row)
 
-        budget.investments.create!(options)
-      rescue ActiveRecord::RecordNotFound => e
-        Rails.logger.error e.message
-        Rails.logger.error e.backtrace
-        next
-      end
+      budget.investments.create!(options)
     end
   end
 
-  def self.perform
-    new.perform
+  def self.import(heading, default_author, file_path)
+    new.import(file_path)
   end
 
   private
 
-  def budget_from_row(row)
-    Budget.find(row['ID'])
-  end
-
-  def heading_from_row(budget, row)
-    group = budget.groups.find_by_name!(row['Group'])
-    group.headings.find_by_name!(row['Heading'])
+  def load_data(file_path)
+    @csv = CSV.parse(File.read(file_path), headers: true)
   end
 
   def author_from_row(row)
-    User.find_by_username! row['Author username']
+    username = row['Author Username']
+    email = row['Email']
+    phone = row['Phone Number']
+
+    return unless username || email
+
+    existing = User.find_by_username(username) || User.find_by_email(email)
+    return existing if existing
+
+    user = User.create(
+      username: username,
+      email: email,
+      phone_number: phone,
+      terms_of_service: '1',
+      skip_password_validation: '1'
+    )
+
+    user.persisted? ? user : nil
   end
 
   def add_map(options, row)
-    lat = row['Latitude'].strip
-    lng = row['Longitude'].strip
+    lat = row['Latitude']
+    lng = row['Longitude']
 
-    if lat.empty? || lng.empty?
+    if lat.nil? || lng.nil?
       options[:skip_map] = '1'
     else
       options[:map_location_attributes] = {
---
+++ spec/lib/budget_idea_importer_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe BudgetIdeaImporter do
+  subject { described_class.new(heading, default_author).import(file_path) }
+
+  let(:heading) { create(:budget_heading) }
+  let(:default_author) { create(:user) }
+  let(:file_path) { Rails.root.join('spec', 'fixtures', 'files', 'idea_import.csv') }
+
+  describe '#import' do
+    it 'creates budget investments (idea)' do
+      expect { subject }.to change { heading.investments.idea.count }.by(3)
+    end
+
+    it 'assigns values' do
+      subject
+      investment = heading.investments.idea.first
+
+      expect(investment.title).to eq('Increase planted trees')
+      expect(investment.description).to eq('Increase planted trees description')
+      expect(investment.location).to eq('Park Slope to Gowanus')
+      expect(investment.map_location.latitude).to eq(40.672014)
+      expect(investment.map_location.longitude).to eq(-73.987251)
+      expect(investment.author.username).to eq('Rex Tang')
+      expect(investment.author.email).to eq('rex.tang@gmail.com')
+      expect(investment.author.phone_number).to eq('917-498-1234')
+    end
+
+    it 'creates map location if present' do
+      expect { subject }.to change { MapLocation.count }.by(2)
+    end
+
+    it 'creates non existing users' do
+      expect { subject }.to change { User.count }.by(2)
+    end
+
+    it 'when user not available, defaults to the provided user' do
+      expect { subject }.to change { default_author.budget_investments.count }.by(1)
+    end
+  end
+end
