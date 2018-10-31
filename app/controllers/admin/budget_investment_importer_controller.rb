d8285cec4da1e5e862479ab1126ccaac590cb195
Ziyan Junaideen <ziyan@jdeen.com>
2018-10-31 17:37:47 +0530

    Controller actions + basic views

--- app/controllers/admin/budget_investment_importer_controller.rb
+++ app/controllers/admin/budget_investment_importer_controller.rb
@@ -1,7 +1,23 @@
 class Admin::BudgetInvestmentImporterController < ApplicationController
   def new
+    @importer = Budget::Investment::Importer.new
   end
 
   def create
+    @importer = Budget::Investment::Importer.new(importer_attributes)
+    if @importer.valid?
+      @importer.import
+      flash[:notice] = 'Import complete'
+      render :report
+    else
+      flash[:notice] = 'Please check form for errors'
+      render :new
+    end
+  end
+
+  private
+
+  def importer_attributes
+    params.require(:budget_investment_importer).permit(:budget_id, :heading_id, :default_author_id, :file)
   end
 end
