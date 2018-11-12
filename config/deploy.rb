14c3cb39e8144f95cd0f02fbf5de1a0d610dbad8
Ziyan Junaideen <ziyan@jdeen.com>
2018-11-12 12:17:25 +0530

    Setting the git repo and request branch - default to current

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,8 @@ set :application, 'consul'
 set :full_app_name, deploysecret(:full_app_name)
 
 set :server_name, deploysecret(:server_name)
-set :repo_url, 'https://github.com/consul/consul.git'
+set :repo_url, 'git@github.com:ziyan-junaideen/consul.git'
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 set :revision, `git rev-parse --short #{fetch(:branch)}`.strip
 
