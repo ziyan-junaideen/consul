2a09f73dbb7f6418e8e0b72844b21758b1fab975
Ziyan Junaideen <ziyan@jdeen.com>
2018-12-05 16:49:57 +0530

    Dev seed ideas enabled by default

--- db/dev_seeds/settings.rb
+++ db/dev_seeds/settings.rb
@@ -50,7 +50,7 @@ section "Creating Settings" do
   Setting.create(key: 'feature.public_stats', value: "true")
   Setting.create(key: 'feature.user.skip_verification', value: "true")
   Setting.create(key: 'feature.help_page', value: "true")
-  Setting.create(key: 'feature.ideas', value: false)
+  Setting.create(key: 'feature.ideas', value: true)
 
   Setting.create(key: 'per_page_code_head', value: "")
   Setting.create(key: 'per_page_code_body', value: "")
