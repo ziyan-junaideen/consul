f9f8913c80a8d90c8fe4e127aab0d6861128d7fd
Ziyan Junaideen <ziyan@jdeen.com>
2018-12-08 19:34:41 +0530

    Show likns wrong state fix

--- app/helpers/budgets_helper.rb
+++ app/helpers/budgets_helper.rb
@@ -22,7 +22,7 @@ module BudgetsHelper
   end
 
   def show_links_to_budget_ideas?(budget)
-    %w[project_forming].include? budget.phase
+    %w[accepting].include? budget.phase
   end
 
   def heading_name_and_price_html(heading, budget)
