57996786b3862107eec3fba3c68187f907483178
Ziyan Junaideen <ziyan@jdeen.com>
2018-12-22 23:28:58 +0530

    Remvoe honeypot / invisble recapcha

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,8 +2,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   prepend_before_action :authenticate_scope!, only: [:edit, :update, :destroy, :finish_signup, :do_finish_signup]
   before_filter :configure_permitted_parameters
 
-  invisible_captcha only: [:create], honeypot: :address, scope: :user
-
   def new
     super do |user|
       user.use_redeemable_code = true if params[:use_redeemable_code].present?
