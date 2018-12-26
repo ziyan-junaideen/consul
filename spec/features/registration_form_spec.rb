013b6a55f0a2cced09a204f26ad5cd44eaa6e131
Ziyan Junaideen <ziyan@jdeen.com>
2018-12-26 17:30:47 +0530

    Removed registraiton honypot related specs

--- spec/features/registration_form_spec.rb
+++ spec/features/registration_form_spec.rb
@@ -42,40 +42,4 @@ feature 'Registration form' do
     expect(new_user.username).to eq("NewUserWithCode77")
     expect(new_user.redeemable_code).to be_nil
   end
-
-  scenario 'Create with invisible_captcha honeypot field' do
-    visit new_user_registration_path
-
-    fill_in 'user_username',              with: "robot"
-    fill_in 'user_address',               with: 'This is the honeypot field'
-    fill_in 'user_email',                 with: 'robot@robot.com'
-    fill_in 'user_password',              with: 'destroyallhumans'
-    fill_in 'user_password_confirmation', with: 'destroyallhumans'
-    check 'user_terms_of_service'
-
-    click_button 'Register'
-
-    expect(page.status_code).to eq(200)
-    expect(page.html).to be_empty
-    expect(page).to have_current_path(user_registration_path)
-  end
-
-  scenario 'Create organization too fast' do
-    allow(InvisibleCaptcha).to receive(:timestamp_threshold).and_return(Float::INFINITY)
-    visit new_user_registration_path
-
-    fill_in 'user_username',              with: "robot"
-    fill_in 'user_address',               with: 'This is the honeypot field'
-    fill_in 'user_email',                 with: 'robot@robot.com'
-    fill_in 'user_password',              with: 'destroyallhumans'
-    fill_in 'user_password_confirmation', with: 'destroyallhumans'
-    check 'user_terms_of_service'
-
-    click_button 'Register'
-
-    expect(page).to have_content 'Sorry, that was too quick! Please resubmit'
-
-    expect(page).to have_current_path(new_user_registration_path)
-  end
-
 end
