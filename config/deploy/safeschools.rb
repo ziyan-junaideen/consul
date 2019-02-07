e8a96cc3074fa427dee4189779d0e02098f948fd
Ziyan Junaideen <ziyan@jdeen.com>
2019-02-07 08:29:54 +0530

    Safeschools

---
+++ config/deploy/safeschools.rb
@@ -0,0 +1,9 @@
+set :repo_url, 'https://github.com/ziyan-junaideen/consul.git'
+set :deploy_to, deploysecret(:deploy_to)
+set :server_name, deploysecret(:server_name)
+set :db_server, deploysecret(:db_server)
+set :ssh_options, port: deploysecret(:ssh_port)
+set :stage, :production
+set :rails_env, :production
+
+server deploysecret(:server1), user: deploysecret(:user), roles: %w(web app db importer cron background)
