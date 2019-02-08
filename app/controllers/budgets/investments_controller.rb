cea0f1fc74e90e6f6bca8a60de1097d89fc8cd61
Ziyan Junaideen <ziyan@jdeen.com>
2019-02-08 17:35:31 +0530

    Vote redirect

--- app/controllers/budgets/investments_controller.rb
+++ app/controllers/budgets/investments_controller.rb
@@ -83,7 +83,7 @@ module Budgets
       @investment.register_selection(current_user)
       load_investment_votes(@investment)
       respond_to do |format|
-        format.html { redirect_to budget_investments_path(heading_id: @investment.heading.id) }
+        format.html { redirect_to budget_investment_path(@investment) }
         format.js
       end
     end
