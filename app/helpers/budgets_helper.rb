69d87b724c6f3697f289eb3cd4b27d120acc50e7
Ziyan Junaideen <ziyan@jdeen.com>
2019-02-08 18:05:57 +0530

    Idea links in selecting

--- app/helpers/budgets_helper.rb
+++ app/helpers/budgets_helper.rb
@@ -22,7 +22,7 @@ module BudgetsHelper
   end
 
   def show_links_to_budget_ideas?(budget)
-    %w[accepting reviewing].include? budget.phase
+    %w[accepting reviewing selecting].include? budget.phase
   end
 
   def heading_name_and_price_html(heading, budget)
