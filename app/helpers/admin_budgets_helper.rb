287857fa4d3bf0c8bef4c4d6010b84fab6ae4a0d
Ziyan Junaideen <ziyan@jdeen.com>
2019-02-27 19:13:50 +0530

    Revert "Budget - create update button text"
    This reverts commit 4ba0f92e7f23a0682b3169ced98cdfb8e0942858.

--- app/helpers/admin_budgets_helper.rb
+++
@@ -1,9 +0,0 @@
-module AdminBudgetsHelper
-  def budget_submit_button_text
-    if @budget.persisted?
-      t('admin.budgets.form.submit.update')
-    else
-      t('admin.budgets.form.submit.create')
-    end
-  end
-end
