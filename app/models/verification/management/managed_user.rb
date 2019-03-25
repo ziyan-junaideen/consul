91373c4522730ec5abb4430ce8c6fbc91d459c12
Ziyan Junaideen <ziyan@jdeen.com>
2019-03-25 10:46:13 +0530

    Storing username as document type and value in session and displaying flash messages

--- app/models/verification/management/managed_user.rb
+++ app/models/verification/management/managed_user.rb
@@ -2,6 +2,8 @@ class Verification::Management::ManagedUser
   include ActiveModel::Model
 
   def self.find(document_type, document_number)
+    return User.find_by_username(document_number) if document_type == 'username'
+
     User.where('document_number is not null').
     find_or_initialize_by(document_type:   document_type,
                           document_number: document_number)
