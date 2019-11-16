ac8dc5db2b6255fcc803f9b1e5730000e38d2415
Ziyan Junaideen <ziyan@jdeen.com>
2019-11-16 17:47:46 +0530

    New Phases Kinds realted test fix

--- spec/features/budgets/budgets_spec.rb
+++ spec/features/budgets/budgets_spec.rb
@@ -205,39 +205,47 @@ describe "Budgets" do
   scenario "Index shows only published phases" do
     budget.update!(phase: :finished)
     phases = budget.phases
-    phases.drafting.update!(starts_at: "30-12-2017", ends_at: "31-12-2017", enabled: true,
+    phases.drafting.update(starts_at: "30-10-2017", ends_at: "31-10-2017", enabled: true,
                            description: "Description of drafting phase",
                            summary: "<p>This is the summary for drafting phase</p>")
 
-    phases.accepting.update!(starts_at: "01-01-2018", ends_at: "10-01-2018", enabled: true,
+    phases.ideas_posting.update(starts_at: "01-11-2017", ends_at: "30-11-2017", enabled: true,
+                           description: "Description of ideas collection phase",
+                           summary: "<p>This is the summary for ideas collection phase</p>")
+
+    phases.project_forming.update(starts_at: "01-12-2017", ends_at: "31-12-2017", enabled: true,
+                           description: "Description of project development phase",
+                           summary: "<p>This is the summary for project development phase</p>")
+
+    phases.accepting.update(starts_at: "01-01-2018", ends_at: "10-01-2018", enabled: true,
                             description: "Description of accepting phase",
                             summary: "This is the summary for accepting phase")
 
-    phases.reviewing.update!(starts_at: "11-01-2018", ends_at: "20-01-2018", enabled: false,
+    phases.reviewing.update(starts_at: "11-01-2018", ends_at: "20-01-2018", enabled: false,
                             description: "Description of reviewing phase",
                             summary: "This is the summary for reviewing phase")
 
-    phases.selecting.update!(starts_at: "21-01-2018", ends_at: "01-02-2018", enabled: true,
+    phases.selecting.update(starts_at: "21-01-2018", ends_at: "01-02-2018", enabled: true,
                             description: "Description of selecting phase",
                             summary: "This is the summary for selecting phase")
 
-    phases.valuating.update!(starts_at: "10-02-2018", ends_at: "20-02-2018", enabled: false,
+    phases.valuating.update(starts_at: "10-02-2018", ends_at: "20-02-2018", enabled: false,
                             description: "Description of valuating phase",
                             summary: "This is the summary for valuating phase")
 
-    phases.publishing_prices.update!(starts_at: "21-02-2018", ends_at: "01-03-2018", enabled: false,
+    phases.publishing_prices.update(starts_at: "21-02-2018", ends_at: "01-03-2018", enabled: false,
                                     description: "Description of publishing prices phase",
                                     summary: "This is the summary for publishing_prices phase")
 
-    phases.balloting.update!(starts_at: "02-03-2018", ends_at: "10-03-2018", enabled: true,
+    phases.balloting.update(starts_at: "02-03-2018", ends_at: "10-03-2018", enabled: true,
                             description: "Description of balloting phase",
                             summary: "This is the summary for balloting phase")
 
-    phases.reviewing_ballots.update!(starts_at: "11-03-2018", ends_at: "20-03-2018", enabled: false,
+    phases.reviewing_ballots.update(starts_at: "11-03-2018", ends_at: "20-03-2018", enabled: false,
                                     description: "Description of reviewing ballots phase",
                                     summary: "This is the summary for reviewing_ballots phase")
 
-    phases.finished.update!(starts_at: "21-03-2018", ends_at: "30-03-2018", enabled: true,
+    phases.finished.update(starts_at: "21-03-2018", ends_at: "30-03-2018", enabled: true,
                            description: "Description of finished phase",
                            summary: "This is the summary for finished phase")
 
